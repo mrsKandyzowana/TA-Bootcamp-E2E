b8c2e65e270568fabe140b650e80a3150c6e85a2
Paulina Kmiecińska <47597328+mrsKandyzowana@users.noreply.github.com>
2022-12-05 18:01:25 +0100

    Updated steps.js

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,39 +1,42 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
+const webPage = 'https://www.newegg.com/';
 
-Given(/^I am on the (\w+) page$/, async (page) => {
-    await browser.url(`https://www.newegg.com/${page}`);
-  });
+Given("I am on the home page", async () => {
+    await browser.url(webPage)
+});
 
 When("I close the promo banner if it appears", async () => {
     try{
-        const promo = await $(`//*[@id="modal-Website"]/div[2]/div/button`).waitForDisplayed({timeout: 2500});
+        const promo = await $(`//*[@id="modal-Website"]/div[2]/div/button`).waitForDisplayed();
         promo.click();
     }catch(error){
         console.error(error);
     }
 });
 
-When ("I enter the word 'Windows' in the search bar", async () => {
-    await $('#app > header > div.page-content-inner > div:nth-child(1) > div.section-left.auto-flex > div.header2021-search.auto-flex > form > div > div.header2021-search-button').click();
+When ("I enter the word {word} in the search bar", async (word) => {
+    const searchIcon = await $(`.header2021-search-button`)
+    const searchInput =  await $(`input[type="search"]`) 
+    await searchIcon.click()
+    await searchInput.setValue(word)
 
-    await $('#app > header > div.page-content-inner > div:nth-child(1) > div.section-left.auto-flex > div.header2021-search.auto-flex.is-active > form > div > div.header2021-search-box.auto-flex > input[type=search]').setValue('Windows');
-   });
+});
 
 When ("I click the search", async () => {
-    await $('#app > header > div.page-content-inner > div:nth-child(1) > div.section-left.auto-flex > div.header2021-search.auto-flex > form > div > div.header2021-search-button').click();
+    await $('//button[contains(., "Search")]').click();
   });
 
 Then("I Check that at least one item appears", async () => {
-    await expect($('#item_cell_32-350-881_1_0')).toBeExisting();
+    await expect($(`.item-cell`)).toBeExisting();
 });
 
 When ("I open 'Today's Best Deals' tab", async () => {
-    const TodayBestDeal = await $('#trendingBanner_720202')
+    const TodayBestDeal = await $(`//*[@id="Portals_swiper"]/div/div[1]/div[2]`)
     await TodayBestDeal.click()
 });   
 
 When ("I click on the Internet shop logo", async () => {
-    const shoplogoImage = await $('/html/body/div[13]/header/div[1]/div[1]/div[1]/div[2]/a')
+    const shoplogoImage = await $(`//*[@id="app"]/header/div[1]/div[1]/div[1]/div[2]`)
     await shoplogoImage.click()
 }); 
 
